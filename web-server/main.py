d369bafaa15f485a6345d2366b8b2e16b1390643
Fidel Olmos <fidelolmos334@gmail.com>
2024-04-16 11:10:56 -0600

    Implement fastAPI in app

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,18 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_contacts():
+    return """
+        <h1>Hola mundo</h1>
+    """
 
 def run():
     store.get_requests()
